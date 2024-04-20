a42e47ad7678b4663a41fe990fffd4e290e36011
chandra575 <kmalware88@gmail.com>
2024-04-20 22:07:14 +0700

    Menghapus Code yang tidak di gunakan

--- TaxCalculator/src/lib/children.java
+++ TaxCalculator/src/lib/children.java
@@ -1,5 +1,30 @@
 package lib;
 
 public class children {
-
+    public class Child {
+        private String name;
+        private String idNumber;
+    
+        public Child(String name, String idNumber) {
+            this.name = name;
+            this.idNumber = idNumber;
+        }
+    
+        // Getter dan setter untuk name dan idNumber
+        public String getName() {
+            return name;
+        }
+    
+        public void setName(String name) {
+            this.name = name;
+        }
+    
+        public String getIdNumber() {
+            return idNumber;
+        }
+    
+        public void setIdNumber(String idNumber) {
+            this.idNumber = idNumber;
+        }
+    }    
 }
